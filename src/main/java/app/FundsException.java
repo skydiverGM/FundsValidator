bfb0072b93f24bfd719737632a471e973b74b04c
skydiverGM <sonytouchscreen@GMAIL.COM>
2024-10-19 18:09:11 +0200

    Init commit

---
+++ src/main/java/app/FundsException.java
@@ -0,0 +1,9 @@
+package app;
+
+// Кастомний виняток
+public class FundsException extends  {
+
+    public FundsException(String message) {
+        super();
+    }
+}
---
+++ src/main/java/app/Main.java
@@ -0,0 +1,45 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static double balance;
+
+    public static void main(String[] args) {
+        balance =
+                validateAmount(balance, getAmount());
+    }
+
+    private static double getBalance() {
+        return 1000.00; // Наявні кошти на рахунку
+    }
+
+    private static double getAmount() {
+        System.out.printf("Balance is USD %.2f.%n" +
+                "Enter purchase amount, USD: ", balance);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+    }
+
+    // Метод валідації наявних коштів
+    private static void validateAmount(double balance, double withdrawal) {
+        if (withdrawal > ) {
+            try {
+                throw new ("Insufficient funds!");
+            } catch (FundsException ex) {
+                System.out.println(.getMessage());
+            }
+        } else {
+            balance = getBalance(balance, withdrawal);
+            System.out.printf("Funds are OK. Purchase paid." +
+                    "%nBalance is USD %.2f", balance);
+        }
+    }
+
+    // Метод розрахунку наявних коштів на рахунку
+    // після зняття певної суми коштів
+    private static double getBalance(double balance, double withdrawal) {
+        return balance - ;
+    }
+}
